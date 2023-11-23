b5ba78d1c9193c1b22f3c17664373d6139497235
kasim kadiwala <96860909+Kasimkkn@users.noreply.github.com>
2023-11-23 09:06:53 +0530

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,86 @@
+import { Link } from "react-router-dom"
+import logo from "../assets/react.svg";
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center h-16 bg-slate-100 absolute w-full bottom-0">
-       <span>All Rights Reserved @2023</span>
-    </footer>
+    <footer className="bg-slate-100 border-y">
+            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+                <div className="md:flex md:justify-between">
+                    <div className="mb-6 md:mb-0">
+                        <Link to="/" className="flex items-center">
+                            <img
+                                src={logo}
+                                className="mr-3 h-16"
+                                alt="Logo"
+                            />
+                        </Link>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4">
+                                    <Link to="/" className="hover:underline">
+                                        Home
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/about" className="hover:underline">
+                                        About
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4">
+                                    <a
+                                        href="https://github.com/Kasimkkn"
+                                        className="hover:underline"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Github
+                                    </a>
+                                </li>
+                                <li>
+                                    <Link to="/" className="hover:underline">
+                                        Discord
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4">
+                                    <Link to="#" className="hover:underline">
+                                        Privacy Policy
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="#" className="hover:underline">
+                                        Terms &amp; Conditions
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <span className="text-sm text-gray-500 sm:text-center">
+                        © 2023 {" "}
+                        <a href="#" className="hover:underline">
+                            Mr KK
+                        </a>
+                        . All Rights Reserved.
+                    </span>
+                    
+                </div>
+            </div>
+        </footer>
+
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,69 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from '../assets/react.svg'
+import logo from '../assets/react.svg';
+
 const Header = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <nav className="w-full flex justify-between items-center bg-slate-100 p-5">
-       <div className="logo">
-        <img src={logo} alt="" />
-       </div>
-       <div className="flex gap-10">
-       <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/contact'>Contact</Link>
-        <Link to='/service'>Service</Link>
-       </div>
-       <div>
-        <Link to='/login'>Login</Link>
-       </div>
-    </nav>
-  )
+    <div>
+      <div className="sm:hidden">
+        <button
+          className="bg-slate-100 p-2"
+          onClick={toggleSidebar}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+      </div>
+
+      <nav className={`w-full sm:w-64 bg-slate-100 p-5 ${isSidebarOpen ? 'block' : 'hidden'}`}>
+        <div className="logo mb-4">
+          <img src={logo} alt="" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Link to='/' onClick={toggleSidebar}>Home</Link>
+          <Link to='/about' onClick={toggleSidebar}>About</Link>
+          <Link to='/contact' onClick={toggleSidebar}>Contact</Link>
+          <Link to='/service' onClick={toggleSidebar}>Service</Link>
+        </div>
+        <div className="mt-4">
+          <Link to='/login' onClick={toggleSidebar}>Login</Link>
+        </div>
+      </nav>
+
+      <div className="hidden sm:flex justify-between items-center bg-slate-100 p-5">
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="flex gap-10">
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+          <Link to='/contact'>Contact</Link>
+          <Link to='/service'>Service</Link>
+        </div>
+        <div>
+          <Link to='/login'>Login</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
