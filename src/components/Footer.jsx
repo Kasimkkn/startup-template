ff21456ee1449e6731ae7c85530942e841123325
kasim kadiwala <96860909+Kasimkkn@users.noreply.github.com>
2023-11-23 08:50:54 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-center items-center h-16 bg-slate-100 absolute w-full bottom-0">
+       <span>All Rights Reserved @2023</span>
+    </footer>
+  )
+}
+
+export default Footer
